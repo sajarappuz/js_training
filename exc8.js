a1ccb4978d9dda91fde9ea0b5d4a05c7780e961d
sajarappuz <sajarmohammed71@gmail.com>
2024-01-01 18:06:12 +0530

    exc10

--- exc8.js
+++ exc8.js
@@ -18,7 +18,7 @@ class BankDetails {
     withdraw(amount){
         if(amount > 0 && amount <= this.balance){
             this.balance-=amount;
-            console.log(`amount withdrawn successfully, ${this.deposit}`)
+            console.log(`amount withdrawn successfully, ${this.balance}`)
         }else{
             console.log("amount is not able to withdraw")
         }
@@ -26,12 +26,28 @@ class BankDetails {
     checkbalance(){
         console.log(`${this.balance}`)
     }
+    transferMoney(targetAccount, amount) {
+        if (amount > 0 && amount <= this.balance) {
+          this.balance -= amount;
+          targetAccount.balance += amount;
+          console.log(`${this.fullName}: Transferred ${amount} to ${targetAccount.fullName}`);
+        } else {
+          console.log("Invalid amount for transfer or insufficient balance.");
+        }
+      }
+    }
 
-}
 const account = new BankDetails("jonny", "james")
+const account1 = new BankDetails("john", "honay")
+
 console.log(account.fullName)
+console.log(account1.fullName)
 account.deposit(3000);
+account1.deposit(2000)
 account.checkbalance()
+account1.checkbalance()
+account.transferMoney(account1, 500)
+account.withdraw(1000)
 
 // class BankDetails {
 //     constructor(firstname, secondname) {
